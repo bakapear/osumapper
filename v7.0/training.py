e257b26ee42e9c47697ebd69724dbc25c70f5d79
bakapear <proxybomb@gmail.com>
2020-11-01 13:04:56 +0100

    add training

---
+++ v7.0/training.py
@@ -0,0 +1,27 @@
+import json
+from act_data_prep import *
+from act_train_rhythm import *
+from act_flow_ds import *
+
+step1_load_maps("../maps.txt")
+
+train_params_p2 = {
+    "divisor": 4,
+    "train_epochs": 16,
+    "train_batch_size": None,
+    "plot_history": True,
+    "too_many_maps_threshold": 200,
+    "train_epochs_many_maps": 6,
+    "data_split_count": 80
+}
+
+model_p2 = step2_build_model()
+
+model_p2 = step2_train_model(model_p2, train_params_p2)
+step2_evaluate(model_p2)
+
+step2_save(model_p2)
+
+flow_dataset_params = step3_set_params(note_group_size=10, step_size=5)
+maps_flow = step3_read_maps_flow(flow_dataset_params)
+step3_save_flow_dataset(maps_flow)
